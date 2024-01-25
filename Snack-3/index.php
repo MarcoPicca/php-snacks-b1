497db6cfc1cf0ab18db739aae4945fdf9d55a7f3
MarcoPicca <il96marco@gmail.com>
2024-01-25 13:14:16 +0100

    snak-3 non visibile in pagina

--- Snack-3/index.php
+++ Snack-3/index.php
@@ -13,16 +13,20 @@ class Pokemon {
         $this->description = $_description;
         $this->principal_attack = $_principal_attack;
         $this->principal_defense = $_principal_defense;
+
+        $newPokemon = new Pokemon();
+
+        $newPokemon -> $_name = 'Toradol';
+        $newPokemon -> $_color = 'purple-white';
+        $newPokemon -> $_description = 'A big Pokèmon in a little world';
+        $newPokemon -> $_principal_attack = 'Throwing Bread';
+        $newPokemon -> $_principal_defense = 'toxic feminism and machismo';
     }
 
-    function get
 }
 
-new $name -> 'Toradol';
-new $color -> 'purple-white';
-new $description -> 'A big Pokèmon in a little world';
-new $principal_attack -> 'Throwing Bread';
-new $principal_defense -> 'toxic feminism and machismo';
 
-echo 'Pokemon';
+
+
+echo Pokemon;
 ?>
