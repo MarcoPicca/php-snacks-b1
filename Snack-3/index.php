53eca55a67a62192f53f1c958db90dc765228ea2
MarcoPicca <il96marco@gmail.com>
2024-01-25 12:47:30 +0100

    snack-3

---
+++ Snack-3/index.php
@@ -0,0 +1,28 @@
+<?php
+
+class Pokemon {
+    public $name;
+    public $color;
+    public $description;
+    public $principal_attack;
+    public $principal_defense;
+
+    public function __construct($_name, $_color, $_description, $_principal_attack, $_principal_defense){
+        $this->name = $_name;
+        $this->color = $_color;
+        $this->description = $_description;
+        $this->principal_attack = $_principal_attack;
+        $this->principal_defense = $_principal_defense;
+    }
+
+    function get
+}
+
+new $name -> 'Toradol';
+new $color -> 'purple-white';
+new $description -> 'A big Pokèmon in a little world';
+new $principal_attack -> 'Throwing Bread';
+new $principal_defense -> 'toxic feminism and machismo';
+
+echo 'Pokemon';
+?>
